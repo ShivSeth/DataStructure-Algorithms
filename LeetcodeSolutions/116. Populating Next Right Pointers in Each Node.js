730f75613765669ee6e6d26569d0f229bb2df14d
Shivam Prakash <shivam.prakash114@gmail.com>
2022-03-13 00:10:35 +0530

    Added more solutions and Algorithm I completed

--- LeetcodeSolutions/116. Populating Next Right Pointers in Each Node.js
+++ LeetcodeSolutions/116. Populating Next Right Pointers in Each Node.js
@@ -12,27 +12,35 @@
  * @param {Node} root
  * @return {Node}
  */
-var connect = function (root) {
-  if (root === null) return root;
-  function bfs(start) {
-    let tempArray = [];
-    let queue = [[start]];
-    while (queue.length) {
-      let nodeArray = queue.shift();
-      nodeArray.forEach((_, i) => {
-        nodeArray[i].next = nodeArray[i + 1];
-        if (i === nodeArray.length - 1) nodeArray[i].next = null;
-      });
-      nodeArray.forEach((node) => {
-        if (node.left && node.right) {
-          tempArray.push(node.left);
-          tempArray.push(node.right);
-        }
-      });
-      queue.push(tempArray);
-      tempArray = [];
-    }
-  }
-  bfs(root);
+var connect = function (root, next = null) {
+  //dfs approach
+  if (!root) return root;
+  root.next = next;
+  connect(root.left, root.right);
+  connect(root.right, root.next?.left);
   return root;
+
+  //below is the bfs approach
+  // if (root === null) return root;
+  // function bfs(start) {
+  //   let tempArray = [];
+  //   let queue = [[start]];
+  //   while (queue.length) {
+  //     let nodeArray = queue.shift();
+  //     nodeArray.forEach((_, i) => {
+  //       nodeArray[i].next = nodeArray[i + 1];
+  //       if (i === nodeArray.length - 1) nodeArray[i].next = null;
+  //     });
+  //     nodeArray.forEach((node) => {
+  //       if (node.left && node.right) {
+  //         tempArray.push(node.left);
+  //         tempArray.push(node.right);
+  //       }
+  //     });
+  //     queue.push(tempArray);
+  //     tempArray = [];
+  //   }
+  // }
+  // bfs(root);
+  // return root;
 };
--- LeetcodeSolutions/62. Unique Paths.js
+++ LeetcodeSolutions/62. Unique Paths.js
@@ -26,3 +26,20 @@
 //       return dp(m - 1, n- 1);
 //   }
 // }
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  let a = Array(m)
+    .fill(1)
+    .map((i) => Array(n).fill(1));
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      a[i][j] = a[i - 1][j] + a[i][j - 1];
+    }
+  }
+  return a[m - 1][n - 1];
+};
